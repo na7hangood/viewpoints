a69b63967e2e66d8a5f7e62d584f2d65c62e14bd
Stephen Wells <stephen.wells@guardian.co.uk>
2016-01-13 08:23:11 -0500

    Fixes case in js import.
    Surprised there weren't more of these TBH. Stupid Mac case preserving file system's to blame here.

--- public/components/fragments/ViewpointList.react.js
+++ public/components/fragments/ViewpointList.react.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
-import SubjectDisplay from './viewpointDisplay.react';
+import SubjectDisplay from './ViewpointDisplay.react';
 
 export default class ViewpointList extends React.Component {
 
